15a37df9a08eafbe7c34edb5c3109188ed2ca998
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2025-02-08 20:58:36 +0000

    download the conexion flask and postgree database

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ from models.user import User
 from models.course import Course
 from models.progress import Progress
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db.init_app(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
+
+# Add a route to verify the app is working
+@app.route('/')
+def home():
+    return "Hello, Neural Odyssey"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -2,6 +2,6 @@ from models import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    usename = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
